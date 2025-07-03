2bc2a5dfa0ba0739dd9592b9ff199830106e59bd
Gonzalo-6 <g-arranz@hotmail.com>
2025-07-03 10:39:09 +0200

    trabajando

---
+++ 1.py
@@ -0,0 +1,25 @@
+
+
+
+
+"""nombre = input("Introduce tu nombre: ")
+edad = input("Introduce tu edad: ")
+print("Hola, " + nombre + " tienes " + edad )"""
+
+""""
+nacionalidad = input("De que nacionalidad eres: ")
+localidad = input("Donde vives: ")
+print(f"Eres {nacionalidad} y vives en {localidad}")
+"""
+
+num1 = float(input("Introduce un número: "))
+num2 = float(input("Intruduce otro número: "))
+suma = num1 + num2
+resta = num1 - num2
+multi = num1 * num2
+divi = num1 / num2
+
+print(f"La suma es {suma}")
+print(f"la resta es {resta}")
+print(f"la división es {divi}")
+print(f"La multiplicación es {multi}")
